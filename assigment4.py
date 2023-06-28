dc54862aade08e5ade8fe82c9fecb73de058ae2d
Vaibhav24y <118295704+Vaibhav24y@users.noreply.github.com>
2023-06-28 19:23:16 +0530

    Add files via upload

---
+++ assigment4.py
@@ -0,0 +1,32 @@
+n=[10,20,30,40]
+for num in n :
+    if num ==60:
+        print("70 found ")
+        break
+else :
+    print(" not found ")
+i=1
+while i<=10:
+    if i ==6:
+        break
+    else :
+        print(i)
+        i+=1
+else :
+    print("break")
+a="Vaibhav"
+for i in a :
+     if i=="h":
+         break
+     else:
+         print(i)
+else :  
+      print("it has pass")     
+for num in range(1,15):
+    if num == 10:
+        continue 
+    print(num)
+for num in range(1,20):
+    if num == 10:
+        pass 
+    print(num)
